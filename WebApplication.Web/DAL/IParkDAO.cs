cf2f57843d87b94af9d0cc4b4652ff2927635fcb
Peter Scanniello <pscanniello216@gmail.com>
2019-03-13 09:54:35 -0400

    Added Park Model and started ParkSqlDAO

---
+++ WebApplication.Web/DAL/IParkDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    interface IParkDAO
+    {
+        Park GetPark(int id);
+
+        IList<Park> GetParks();
+    }
+}
---
+++ WebApplication.Web/DAL/ParkSqlDAO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public class ParkSqlDAO : IParkDAO
+    {
+        private string connectionString;
+        public ParkSqlDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public Park GetPark(int id)
+        {
+            Park park = new Park();
+
+
+
+
+
+        }
+
+        public IList<Park> GetParks()
+        {
+            IList<Park> parks = new List<Park>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM park", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Park park = ConvertReaderToPark(reader);
+                        parks.Add(park);
+                    }
+                }
+             }
+            catch (SqlException ex)
+            {
+
+                throw;
+            }
+            return parks;
+        }
+
+        private Park ConvertReaderToPark(SqlDataReader reader)
+        {
+            Park park = new Park();
+
+            park.ParkCode = Convert.ToString(reader["ParkCode"]);
+            park.ParkName = Convert.ToString(reader["ParkName"]);
+            park.State = Convert.ToString(reader["State"]);
+            park.Acreage = Convert.ToInt32(reader["Acreage"]);
+            park.ElevationInFeet = Convert.ToInt32(reader["ElevationInFeet"]);
+            park.MilesOfTrail = Convert.ToInt32(reader["MilesOfTrail"]);
+            park.NumberOfCampsites = Convert.ToInt32(reader["NumberOfCampsites"]);
+            park.Climate = Convert.ToString(reader["Climate"]);
+            park.YearFounded = Convert.ToInt32(reader["YearFounded"]);
+            park.AnnualVisitorCount = Convert.ToInt32(reader["AnnualVisitorCount"]);
+            park.InspirationalQuote = Convert.ToString(reader["InspirationalQuote"]);
+            park.ParkDescription = Convert.ToString(reader["ParkDescription"]);
+            park.EntryFee = Convert.ToDecimal(reader["EntryFee"]);
+            park.NumberOfAnimalSpecies = Convert.ToInt32(reader["NumberOfAnimalSpecies"]);
+
+            return park;
+        }
+    }
+}
